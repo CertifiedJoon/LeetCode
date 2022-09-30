dcfb8c33dd4933dee7ac8735873936d9eb942c61
CertifiedJoon <mjypark1212@gmail.com>
2022-09-30 15:36:29 +0800

    Day 2 Done

---
+++ isSubsequence.py
@@ -0,0 +1,14 @@
+def isSubsequence(s,t):
+    i = j = 0
+    while i < len(s):
+        while j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+                break
+            j += 1
+        if j == len(t) and i < len(s):
+            return False
+    return True
+
+print(isSubsequence('axe', 'abcde'))
--- isomorphic.py
+++ isomorphic.py
@@ -1,16 +1,30 @@
 def isIsomorphic(s, t):
-    char_map = {}
+    char_map = {} # char mapping
+    mapped_char = set() # set(char_map.values())
     for x, y in zip(s,t):
-        if x not in char_map and y not in char_map:
+        if x not in char_map and y not in mapped_char:
             char_map[x] = y
-            char_map[y] = x
-        elif x in char_map and y in char_map:
-            if char_map[x] != y or char_map[y] != x:
-                return False
-        else:
+            mapped_char.add(y)
+        elif not (x in char_map and char_map[x] == y):
             return False
     
     return True
 
+def isIsomorphicWords(words):
+    """
+    Extension to above problem.
+    Same problem but with a group of strings instead of two.
+    """
+    #First Occurence Trasformation.
+    transformed = set() # stores all transformed strings as a set
+    for word in words:
+        first_seen = {} # maps character to the index at which the char was seen for the first time
+        occurence = [0] * len(word) # transformed version of each word
+        for i, c in enumerate(word):
+            if c not in first_seen:
+                first_seen[c] = i
+            occurence[i] = str(first_seen[c])
+        transformed.add(' '.join(occurence))
+    return len(transformed) <= 1
 
-print(isIsomorphic("paper", "title"))
+print(isIsomorphicWords([]))
