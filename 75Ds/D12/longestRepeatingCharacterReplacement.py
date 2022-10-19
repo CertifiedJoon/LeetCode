6bf9befb849457757c35ee053d5ecf62ba9591b1
CertifiedJoon <mjypark1212@gmail.com>
2022-10-19 10:21:55 +0800

    residual work done

---
+++ 75Ds/D12/longestRepeatingCharacterReplacement.py
@@ -0,0 +1,15 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        frequency = defaultdict(int)
+        longest_len = largest_freq = 0
+        for r in range(len(s)):
+            frequency[s[r]] += 1
+            curr_len = r - l + 1
+            largest_freq = max(frequency[s[r]], largest_freq)
+            if curr_len - largest_freq <= k:
+                longest_len = max(longest_len, curr_len)
+            else:
+                frequency[s[l]] -= 1
+                l += 1
+        return longest_len
