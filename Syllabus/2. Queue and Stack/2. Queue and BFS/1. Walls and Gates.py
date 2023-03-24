ee021e4e3ee89deeabb3fe37e5eedb4bb96ded6f
Joon_Thinkpad <mjypark1212@gmail.com>
2023-03-24 10:21:23 +0800

    Walls N Gate debut

--- Syllabus/2. Queue and Stack/2. Queue and BFS/1. Walls and Gates.py
+++ Syllabus/2. Queue and Stack/2. Queue and BFS/1. Walls and Gates.py
@@ -1,27 +1,30 @@
 from collections import deque
-import typing
+from typing import List, Tuple, Set
 
 def WallsnGates(rooms: List[List[int]]) -> None:
   for row in range(len(rooms)):
-    for col in range(len(rooms)):
+    for col in range(len(rooms[0])):
       visited = set()
       q = deque()
-      if rooms[row][col] == 0:
+      if rooms[row][col] == 2147483647:
         q.append((row, col))
       
       while q:
         i, j = q.popleft()
         visited.add((i, j))
         if rooms[i][j] == 0:
-          rooms[row][col] == abs(row - i) + abs(col - j)
+          rooms[row][col] = abs(row - i) + abs(col - j)
           break
         elif rooms[i][j] != -1:
-          nbs = ((i + 1, j + 1), (i + 1, j - 1), (i - 1, j + 1), (i - 1, j - 1))
+          nbs = ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1))
           for nb in nbs:
             if is_valid_neighbor(nb, visited, rooms):
               q.append(nb)
 
-def is_valid_neighbor(nb, visited, rooms) -> bool:
+def is_valid_neighbor(nb: Tuple, visited: Set, rooms: List[List[int]]) -> bool:
     row, col = nb
-    return row >= 0 and row < len(rooms) and col >= 0 and col < len(rooms[0]) and rooms[row][col] != -1 and (nb not in rooms)
+    return row >= 0 and row < len(rooms) and col >= 0 and col < len(rooms[0]) and rooms[row][col] != -1 and (nb not in visited)
     
+rooms = [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
+WallsnGates(rooms)
+print(rooms)
