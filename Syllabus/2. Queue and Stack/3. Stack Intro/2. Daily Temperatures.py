c5b5dc4c4250fe637cd4c78e21af0649ce9c86a9
Joon_Thinkpad <mjypark1212@gmail.com>
2023-03-30 10:20:14 +0800

    daily temperatures dp

--- Syllabus/2. Queue and Stack/3. Stack Intro/2. Daily Temperatures.py
+++ Syllabus/2. Queue and Stack/3. Stack Intro/2. Daily Temperatures.py
@@ -1,4 +1,4 @@
-def daily_temperatures(temperatures):
+def daily_temperatures_monotonic(temperatures):
   answers = [0 for _ in range(len(temperatures))]
   search_stack = []
   for i in range(len(temperatures)):
@@ -8,4 +8,21 @@ def daily_temperatures(temperatures):
     search_stack.append(i)
   return answers
 
-print(daily_temperatures([30,40,50,60]))
+def daily_temperatures_top_down(temperatures):
+  n = len(temperatures)
+  hottest = 0
+  answer = [0] * n
+
+  for curr_day in range(n - 1, -1, -1):
+    current_temp = temperatures[curr_day]
+    if current_temp >= hottest:
+      hottest = current_temp
+      continue
+    days = 1
+    while temperatures[curr_day + days] <= current_temp:
+      # Use information from answer to search for the next warmer day
+      days += answer[curr_day + days]
+      answer[curr_day] = days
+    return answer
+
+print(daily_temperatures_monotonic([30,40,50,60]))
