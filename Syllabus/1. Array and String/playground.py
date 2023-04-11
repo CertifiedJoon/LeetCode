4916b513085f3c9e1366d434651120606812bf1d
Joon_Thinkpad <mjypark1212@gmail.com>
2023-04-11 08:55:21 +0800

    playground.py

--- Syllabus/1. Array and String/playground.py
+++ Syllabus/1. Array and String/playground.py
@@ -1,22 +1,25 @@
-class StackQueue:
+class MinStack:
   def __init__(self):
-    self._pop_stack = []
-    self._push_stack = []
+    self._min_stack = [] # [(latestMin, count)] works as a timestamp
+    self._stack = []
 
   def push(self, val: int) -> None:
-    self._push_stack.append(val)
+    self._stack.append(val)
+    if not self._min_stack or val < self._min_stack[-1][0]:
+      self._min_stack((val, 1))
+    elif val == self._min_stack[-1][0]:
+      self._min_stack[-1][1] += 1
   
   def pop(self) -> int:
-    if not self._pop_stack:
-      while self._push_stack:
-        self._pop_stack.append(self._push_stack.pop())
-    return self._pop_stack.pop()
-
-  def peek(self) -> int:
-    if not self._pop_stack:
-      while self._push_stack:
-        self._pop_stack.append(self._push_stack.pop())
-    return self._pop_stack[-1]
+    popped = self._stack.pop()
+    if popped == self._min_stack[-1][0]:
+      self._min_stack[-1][1] -= 1
+    if self._min_stack[-1][1] == 0:
+      self._min_stack.pop()
+    return popped
   
-  def empty(self) -> bool:
-    return not (self._pop_stack and self._push_stack)
+  def top(self) -> int:
+    return self._stack[-1]
+  
+  def getMin(self) -> int:
+    return self._min_stack[-1][0]
