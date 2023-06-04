4f320d62d472123f417da1b5d01a21dbd1cdf31d
Joon_Thinkpad <mjypark1212@gmail.com>
2023-06-04 10:50:36 +0800

    jewels and stone

---
+++ Syllabus/3. Hash Map/4. Conclustion/2. Longest Substring Without Repeating Character.py
@@ -0,0 +1,17 @@
+def longest_uniq_subtring(s: str) -> int:
+  find = dict()
+  longest_len = 0
+  curr_len = 0
+  for i, c in enumerate(s):
+    if c not in find:
+      find[c] = i
+      curr_len += 1
+    else:
+      curr_len = i - find[c]
+      find[c] = i
+
+    if curr_len > longest_len:
+      longest_len = curr_len
+
+    return longest_len
+    
