859fa153f27cfcc303b47e777b5311c9ae0ed89b
Joon_Thinkpad <mjypark1212@gmail.com>
2023-07-18 08:45:29 +0800

    Add: kth largest In Stream

--- Syllabus/5. Heap/KthLargestInStream.py
+++ Syllabus/5. Heap/KthLargestInStream.py
@@ -15,3 +15,18 @@ class KthLargest:
             heapq.heappop(self.heap)
 
         return self._heap[0]
+
+
+class KthLargest2:
+    def __init__(self, k: int, nums: list[int]) -> None:
+        self._heap = heapq.heapify([-num for num in nums])
+        self._k = k
+
+        while len(self._heap) > k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int):
+        heapq.heappush(self._heap, -val)
+        if len(self._heap) > self._k:
+            heapq.heapop(self.heap)
+        return -self._heap[0]
