9dffe9dd2b8a5c9377bf44e5871a168e9e237965
Joon_Thinkpad <mjypark1212@gmail.com>
2023-09-10 16:52:14 +0800

    playgrounding

--- playground.py
+++ playground.py
@@ -1,45 +1,24 @@
-ss = ["james", "John", "janE", "joon"]
-ss.sort(key=str.lower, reverse=True)
-print(ss)
+n = 257
+n -= 1
 
-mapped = {1: "a", 2: "b", 3: "c"}
-print(mapped.get(4, "d"))
-print(mapped.pop(4, "d"))
-print(mapped.popitem())
-print(mapped.update(mapped))
-print(mapped | mapped)
-mapped |= mapped
+print(bin(n))
+n |= n >> 1
 
-nums1 = [1, 2, 3, 4]
-nums2 = [2, 3, 4, 5]
+print(bin(n))
+n |= n >> 2
 
+print(bin(n))
+n |= n >> 4
 
-def is_greater(a, b):
-    return a > b
+print(bin(n))
+n |= n >> 8
 
+print(bin(n))
+n |= n >> 16
 
-print(list(map(is_greater, nums1, nums2)))
+print(bin(n))
+n |= n >> 32
 
+n += 1
 
-name = "   Joonyoung Moon   "
-
-print(name.index("oo", 4))
-print(name.isalnum())
-print(name.isalpha())
-print(name.isascii())
-print(name.isdecimal())
-print(name.isdigit())
-print(name.islower())
-print(name.isnumeric())
-print(name.isupper())
-print(name.lstrip())
-print(name.removeprefix("Joonyoung"))
-print(name.removesuffix("Moon"))
-print(name.replace("oo", "00"))
-print(name.split("oo"))
-print(name.splitlines())
-print(name.strip("wcmowz"))
-
-ss.sort(key=str.lower)
-
-print(ss)
+print(n)
