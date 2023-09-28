5e4860c24c91e1ebbc40ec4fdd90f68d513baece
Joon_Thinkpad <mjypark1212@gmail.com>
2023-09-28 09:57:44 +0800

    kmp

--- Syllabus/7. Interview Question/rotate_string.py
+++ Syllabus/7. Interview Question/rotate_string.py
@@ -1,27 +1,43 @@
 class Solution:
+    def kmp(self, A, B):
+        i, j = 0, 0
+        lps = self.lps(B)
+        while i < len(A):
+            if A[i] == B[j]:
+                i += 1
+                j += 1
+
+            if j == len(B):
+                return i - j
+
+            if A[i] != B[j] and i < len(A):
+                if j:
+                    j = lps[j - 1]
+                else:
+                    i += 1
+
+    def lps(self, B):
+        len = 0
+        i = 1
+        lps_table = [0 for _ in range(len(B))]
+
+        while i < len(B):
+            if B[len] == B[i]:
+                len += 1
+                lps_table[i] = len
+                i += 1
+            elif len:
+                len = lps_table[len - 1]
+            else:
+                i += 1
+
+        return lps_table
+
     def rotateString(self, A, B):
         N = len(A)
         if N != len(B):
             return False
         if N == 0:
             return True
-
-        shifts = [1] * (N + 1)
-        left = -1
-
-        for right in range(N, -1, -1):
-            while left >= 0 and B[left] != B[right]:
-                left -= shifts[left]
-            shifts[right + 1] = right - left
-            left += 1
-
-        match_len = 0
-        for char in A + A:
-            while match_len >= 0 and B[match_len] != char:
-                match_len -= shifts[match_len]
-
-            match_len += 1
-            if match_len == N:
-                return True
 
         return False
