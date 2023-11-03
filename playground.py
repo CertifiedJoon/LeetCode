caa82d9f46d8a2822aa83040eaefff6abd2e3058
Joon_Thinkpad <mjypark1212@gmail.com>
2023-11-03 15:03:47 +0800

    wip

--- playground.py
+++ playground.py
@@ -1,51 +1,15 @@
-class Node:
-    def __init__(self, letter):
-        self._letter = letter
-        self._children = dict()
+def generateParentheses(n: int):
+    para = []
 
-    def add_child(self, c):
-        node = Node(c)
-        self._children[c] = node
-        return node
+    if n == 0:
+        return [""]
 
-    def get_letter(self):
-        return self._letter
+    for left in range(n):
+        for left_string in generateParentheses(left):
+            for right_string in generateParentheses(n - 1 - left):
+                para.append("(" + left_string + ")" + right_string)
 
-    def iter_children(self):
-        for child in self._children:
-            yield child
-
-    def is_parent(self, letter):
-        return letter in self._children
-
-    def get_child(self, letter):
-        return self._children[letter]
+    return para
 
 
-class Trie:
-    def __init__(self, words):
-        self._root = Node("_")
-
-        for word in words:
-            curr = self._root
-            for c in word:
-                if curr.is_parent(c):
-                    curr = curr.get_child(c)
-                else:
-                    curr = curr.add_child(c)
-
-    def search(self, word):
-        curr = self._root
-        matched_chars = []
-
-        for c in word:
-            if curr.is_parent(c):
-                matched_chars.append(c)
-                curr = curr.get_child(c)
-
-        return "".join(matched_chars)
-
-
-if __name__ == "__main__":
-    trie = Trie(["mobile", "mouse", "moneypot", "monitor", "mousepad"])
-    print(trie.search("mouse"))
+print(generateParentheses(4))
