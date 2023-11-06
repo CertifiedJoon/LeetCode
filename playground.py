4a2fb18b9528f740fc32b905f677c5f1146f8fa9
Joon_Thinkpad <mjypark1212@gmail.com>
2023-11-06 20:02:30 +0800

    wip

--- playground.py
+++ playground.py
@@ -1,39 +1,15 @@
-"""
-Premise:
-1. given integer, return the least number of perfect square numbers that sum to n.
+from typing import List
 
-Constaints:
-1. 1 <= n <= 10^4
-
-first level
-12 -> [1, 4, 9] 
-
-second level
-11 -> [1, 4, 9]
-8 -> [1, 4]
-3 -> [1]
-
-"""
-
-import math
+import heapq
 
 
-def squareSum(n: int):
-    # queue implemented in set to reduce redundancy
-    queue = set()
-    queue.add(n)
+def lastStoneWeight(stones: List[int]) -> int:
+    stones = [-stone for stone in stones]
+    heapq.heapify(stones)
 
-    level = 0
+    while len(stones) > 1:
+        stone_one, stone_two = -heapq.heappop(stones), -heapq.heappop(stones)
+        new_stone = abs(stone_one - stone_two)
+        heapq.heappush(stones, -new_stone)
 
-    while queue:
-        newQueue = set()
-        for elem in queue:
-            if elem == 0:
-                return level
-            for root in range(1, math.sqrt(elem)):
-                if elem - root**2 > 0:
-                    newQueue.add(elem - root**2)
-        queue = newQueue
-        level += 1
-
-    raise RuntimeError("Error Square sum impossible.")
+    return -stones[0]
