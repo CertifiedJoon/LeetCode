698c62597084f3f24a56070c9fbfa792b2de35f5
Joon_Thinkpad <mjypark1212@gmail.com>
2024-08-08 17:21:49 +0900

    LeetCodePracticePush

--- history/perfectSquares.py
+++ history/perfectSquares.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numSquares(self, n: int) -> int:
+    def numSquaresDP(self, n: int) -> int:
         """
         Premise
         . given an integer, return the least number of square numbers that sum to number
@@ -22,3 +22,32 @@ class Solution:
             leastSquareSum[i] = 1 + minLeastSquare
 
         return leastSquareSum[n]
+
+    def numSquaresOptimizedBfs(self, n: int) -> int:
+        """
+        Premise
+        . given an integer, return the least number of square numbers that sum to number
+
+        Constraint
+        . 1 <= n <= 10000
+        """
+        squareNums = [i * i for i in range(1, int(n**0.5) + 1)]
+        level = 0
+        queue = {n}
+
+        while queue:
+            level += 1
+            nextQueue = set()
+
+            for remainder in queue:
+                for squareNum in squareNums:
+                    if remainder == squareNum:
+                        return level
+                    elif remainder < squareNum:
+                        break
+                    else:
+                        nextQueue.add(remainder - squareNum)
+
+            queue = nextQueue
+
+        return level
