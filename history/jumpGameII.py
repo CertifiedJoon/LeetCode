66f2a9d6c26a12713e1bbdef7646b837b2b64ad1
Joon_Thinkpad <mjypark1212@gmail.com>
2024-08-13 11:21:09 +0900

    LeetCodePracticePush

--- history/jumpGameII.py
+++ history/jumpGameII.py
@@ -17,3 +17,30 @@ class Solution:
                 dp[i + jump] = min(dp[i + jump], dp[i] + 1)
 
         return dp[-1]
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        """
+        Premise
+        . given nums, return the minimum number of jumps to reach the end.
+
+        Constraint
+        . 0 <= nums[i] <= 1000
+        """
+        i = 0
+        jumpCnt = 0
+        while i != len(nums) - 1:
+            maxJump = nums[i]
+            nextI = i
+            m = 0
+            for jump in range(1, maxJump + 1):
+                if (i + jump) >= len(nums) - 1:
+                    nextI = len(nums) - 1
+                elif (jump + nums[i + jump]) > m:
+                    m = jump + nums[i + jump]
+                    nextI = jump + i
+            jumpCnt += 1
+            i = nextI
+
+        return jumpCnt
