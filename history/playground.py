6eb3234458db8f64f0e3a1e0f1b893561d20086d
Joon_Thinkpad <mjypark1212@gmail.com>
2024-08-14 13:28:44 +0900

    LeetCodePracticePush

--- history/playground.py
+++ history/playground.py
@@ -27,3 +27,59 @@ class Solution:
                 jumpCount += 1
 
         return jumpCount
+
+
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        """
+        Premise
+        . given nums, return the max gap between two successive elements in sorted form
+
+        Constrating
+        . nums are non negative <= 10^9
+        . algo must be linear time and linear space
+        """
+        if 2 > len(nums):
+            return 0
+
+        class Bucket:
+            def __init__(self):
+                self.max = -inf
+                self.min = inf
+                self.length = 0
+
+            def put(self, val):
+                self.max = max(val, self.max)
+                self.min = min(val, self.min)
+                self.length += 1
+
+            def isEmpty(self):
+                return self.length == 0
+
+            def getGap(self):
+                return self.max - self.min
+
+        maximum = max(nums)
+        minimum = min(nums)
+        n = len(nums)
+        bucketSize = (maximum - minimum) // n + 1
+        bucketCount = (maximum - minimum) // bucketSize + 1
+        buckets = [Bucket() for _ in range(bucketCount)]
+        maxGap = 0
+        prevMax = None
+
+        for num in nums:
+            buckets[(num - minimum) // bucketSize].put(num)
+
+        for bucket in buckets:
+            if bucket.isEmpty():
+                continue
+
+            if not prevMax:
+                maxGap = max(maxGap, bucket.getGap())
+            else:
+                maxGap = max(maxGap, bucket.getGap(), bucket.min - prevMax)
+
+            prevMax = bucket.max
+
+        return maxGap
