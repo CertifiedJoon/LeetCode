1635e1efe4d38f393207ce64eddd5db31c111348
Joon_Thinkpad <mjypark1212@gmail.com>
2024-08-21 12:01:16 +0900

    LeetCodePracticePush

--- history/meetingRoomsII.py
+++ history/meetingRoomsII.py
@@ -15,3 +15,29 @@ class Solution:
             maxMeetingRoomCnt = max(meetingRoomCnt, maxMeetingRoomCnt)
 
         return maxMeetingRoomCnt
+
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if not intervals:
+            return 0
+
+        usedRooms = 0
+
+        starts = sorted([interval[0] for interval in intervals])
+        ends = sorted([interval[1] for interval in intervals])
+
+        l = len(intervals)
+
+        endPointer = 0
+        startPointer = 0
+
+        while startPointer < l:
+            if starts[startPointer] >= ends[endPointer]:
+                usedRooms -= 1
+                endPointer += 1
+
+            usedRooms += 1
+            startPointer += 1
+
+        return usedRooms
