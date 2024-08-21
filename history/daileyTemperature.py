c7507a6306d3a13b458615dfa0e5d7f35e6a32ca
Joon_Thinkpad <mjypark1212@gmail.com>
2024-08-21 11:19:57 +0900

    LeetCodePracticePush

--- history/daileyTemperature.py
+++ history/daileyTemperature.py
@@ -10,3 +10,26 @@ class Solution:
             colderStack.append((i, temperature))
 
         return answer
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0 for _ in range(len(temperatures))]
+
+        if len(temperatures) < 2:
+            return answer
+
+        for i in range(len(temperatures) - 2, -1, -1):
+            jump = 1
+            start = i
+            while temperatures[start + jump] <= temperatures[i]:
+                if answer[start + jump] == 0:
+                    break
+                start += jump
+                jump = answer[start]
+            if temperatures[start + jump] <= temperatures[i]:
+                answer[i] = 0
+            else:
+                answer[i] = start + jump - i
+
+        return answer
